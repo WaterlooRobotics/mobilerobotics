c06ec8f1427a54c1a4a208b3e8c4e4aa5352b58f
Chris Choi <chutsu@gmail.com>
2016-02-28 21:05:09 -0500

    Refactor resampling.m it is now B-E-A-U-TIFUL

--- matlab_simulation/05-estimation/particle_filter/resampling.m
+++ matlab_simulation/05-estimation/particle_filter/resampling.m
@@ -1,59 +1,27 @@
+% resampling parameters
 M = 200;
 X = rand(M, 2);
 W = [1:M] ./ M;
 
 for t = 1:200
     % particle filter estimation
-    for m=1:M
+    for m = 1:M
         seed = rand(1);
-        X(m,:,t+1) = X(find(W>seed,1),:,t);
+        X(m, :, t + 1) = X(find(W > seed, 1), :, t);
     end
 end
 
+
+% plot resampling of particles
+t_range = [1, 2, 5, 10, 20, 30, 50, 70, 200];
 figure(1);
 clf;
 
-subplot(3,3,1)
-plot(X(:,1,1),X(:,2,1),'ro')
-axis([0 1 0 1]);
-xlabel('t=1')
-
-subplot(3,3,2)
-plot(X(:,1,2),X(:,2,2),'ro')
-axis([0 1 0 1]);
-xlabel('t=2')
-
-subplot(3,3,3)
-plot(X(:,1,5),X(:,2,5),'ro')
-axis([0 1 0 1]);
-xlabel('t=5')
-
-subplot(3,3,4)
-plot(X(:,1,10),X(:,2,10),'ro')
-axis([0 1 0 1]);
-xlabel('t=10')
-
-subplot(3,3,5)
-plot(X(:,1,20),X(:,2,20),'ro')
-axis([0 1 0 1]);
-xlabel('t=20')
-
-subplot(3,3,6)
-plot(X(:,1,30),X(:,2,30),'ro')
-axis([0 1 0 1]);
-xlabel('t=30')
-
-subplot(3,3,7)
-plot(X(:,1,50),X(:,2,50),'ro')
-axis([0 1 0 1]);
-xlabel('t=50')
-
-subplot(3,3,8)
-plot(X(:,1,70),X(:,2,70),'ro')
-axis([0 1 0 1]);
-xlabel('t=70')
-
-subplot(3,3,9)
-plot(X(:,1,200),X(:,2,200),'ro')
-axis([0 1 0 1]);
-xlabel('t=200')
+for plot_index = 1:length(t_range)
+    t = t_range(plot_index);
+    
+    subplot(3, 3, plot_index);
+    plot(X(:, 1, t), X(:, 2, t),'ro');
+    axis([0 1 0 1]);
+    xlabel(['t = ' num2str(t)]);
+end
