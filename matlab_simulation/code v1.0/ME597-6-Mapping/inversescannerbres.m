5fe78cb528e8f84cc0282793833e0716539c58e8
Ian Colwell <icolwell@gsd.uwaterloo.ca>
2016-03-02 09:12:30 -0500

    Work in progress, very rough restructering

--- matlab_simulation/code v1.0/ME597-6-Mapping/inversescannerbres.m
+++ matlab_simulation/code v1.0/ME597-6-Mapping/inversescannerbres.m
@@ -2,17 +2,34 @@ function [m] = inversescannerbres(M,N,x,y,theta,r,rmax)
 % Calculates the inverse measurement model for a laser scanner through
 % raytracing with Bresenham's algorithm, assigns low probability of object
 % on ray, high probability at end.  Returns cells and probabilities.
+% Format:
+%   [m] = inversescannerbres(M,N,x,y,theta,r,rmax)
+% Input:
+%   M = Total Height of map
+%   N = Total Width of map
+%   x = Robot x position
+%   y = Robot y position
+%   theta = Angle of laser
+%   r = Range to measured object
+%   rmax = Max range of laser
 
 % Range finder inverse measurement model
+
+% Bound the robot within the map dimensions
 x1 = max(1,min(M,round(x)));
 y1 = max(1,min(N,round(y)));
 
+% Calculate position of measured object (endpoint of ray)
 endpt = [x y] + r*[cos(theta) sin(theta)];
 
+% Bound the endpoint within the map dimensions
 x2 = max(1,min(M,round(endpt(1))));
 y2 = max(1,min(N,round(endpt(2))));
 
+% Get coordinates of all cells traversed by laser ray 
 [list(:,1) list(:,2)] = bresenham(x1,y1,x2,y2);
+
+% Assign probabilities
 m = [list 0.4*ones(length(list),1)];
 if (r<rmax)
     m(end,3) = 0.6;
