47ccd45d5e2540390c6909650d07a4db75ff1ecd
Chris Choi <chutsu@gmail.com>
2016-03-02 17:37:44 -0500

    Refactor out bearing and range calculations out of particle localization

--- matlab_simulation/06-mapping/particle_localization/particle_localization.m
+++ matlab_simulation/06-mapping/particle_localization/particle_localization.m
@@ -32,13 +32,13 @@ R = [0.0001 0 0;
 [RE, Re] = eig(R);
 
 % Measurement type and noise
-meas = 3; % 1 - range, 2 - bearing, 3 - both
+meas = 3;
 switch(meas)
-case 1
+case 1  % 1 - range
     Q = 0.01;
-case 2
+case 2  % 2 - bearing
     Q = 0.01;
-case 3
+case 3  % 3 - both
     Q = [0.01, 0; 0, 0.01];
 end
 [QE, Qe] = eig(Q);
@@ -67,91 +67,79 @@ x(:, 1) = x0;
 m = length(Q(:, 1));
 y = zeros(m, length(T));
 mf = zeros(2, length(T));
+muParticle_S = zeros(1, D, length(T));
 
-%plot_initial_state(1, map, x, X, D);
+% plot_initial_state(1, map, x, X, D);
 
 
-% % % Main loop
-% for t=2:length(T)
-%     % Simulation
-%     % Select a motion disturbance
-%     e = RE*sqrt(Re)*randn(n,1);
-%     % Update state
-%     x(:,t) = [x(1,t-1)+u(1,t)*cos(x(3,t-1))*dt;
-%               x(2,t-1)+u(1,t)*sin(x(3,t-1))*dt;
-%               x(3,t-1)+u(2,t)*dt] + e;
-%     
-%     % Take measurement
-%     % Pick feature
-%     mf(:,t) = closestfeature(map,x(:,t));
-%     % Select a motion disturbance
-%     d = QE*sqrt(Qe)*randn(m,1);
-%     % Determine measurement
-%     switch(meas) 
-%         case 1
-%             y(:,t) = [sqrt((mf(1,t)-x(1,t))^2 + (mf(2,t)-x(2,t))^2)] + d;
-%         case 2 
-%             y(:,t) = [mod(atan2(mf(2,t)-x(2,t),mf(1,t)-x(1,t))-x(3,t)+pi,2*pi)-pi] + d;
-%         case 3
-%             y(:,t) = [sqrt((mf(1,t)-x(1,t))^2 + (mf(2,t)-x(2,t))^2);
-%                       mod(atan2(mf(2,t)-x(2,t),mf(1,t)-x(1,t))-x(3,t)+pi,2*pi)-pi] + d;
-%     end
-%     % Particle filter estimation
-%     for dd=1:D
-%         e = RE*sqrt(Re)*randn(n,1);
-%         Xp(:,dd) = [X(1,dd)+u(1,t)*cos(X(3,dd))*dt;
-%                     X(2,dd)+u(1,t)*sin(X(3,dd))*dt;
-%                     X(3,dd)+u(2,t)*dt] + e;
-%         switch(meas)
-%             case 1
-%                 hXp = [sqrt((mf(1,t)-Xp(1,dd))^2 + (mf(2,t)-Xp(2,dd))^2)] + d;
-%             case 2
-%                 hXp = [mod(atan2(mf(2,t)-Xp(2,dd),mf(1,t)-Xp(1,dd))-Xp(3,dd)+pi,2*pi)-pi] + d;
-%             case 3
-%                 hXp = [sqrt((mf(1,t)-Xp(1,dd))^2 + (mf(2,t)-Xp(2,dd))^2);
-%                         mod(atan2(mf(2,t)-Xp(2,dd),mf(1,t)-Xp(1,dd))-Xp(3,dd)+pi,2*pi)-pi] + d;
-%         end
-%         w(dd) = max(1e-8,mvnpdf(y(:,t),hXp,Q));
-%     end
-%     W = cumsum(w);
-%     for dd = 1:D
-%         seed = max(W) * rand(1);
-%         X(:, dd) = Xp(:, find(W > seed, 1));
-%     end
-%     muParticle = mean(X);
-%     SParticle = var(X);
-%     muParticle_S(:, dd) = muParticle;
-%     
-%     % Plot results
-%     figure(1);
-%     clf; 
-%     hold on;
-%     plot(map(:,1),map(:,2),'go', 'MarkerSize',10,'LineWidth',2);
-%     plot(mf(1,t),mf(2,t),'mx', 'MarkerSize',10,'LineWidth',2)
-%     plot(x(1,1:t),x(2,1:t), 'ro--')
-%     if (meas == 1) 
-%         circle(1,x(1:2,t), y(1,t)); 
-%     end
-%     if (meas == 2) 
-%         plot([x(1,t) x(1,t)+10*cos(y(1,t)+x(3,t))], [ x(2,t) x(2,t)+10*sin(y(1,t)+x(3,t))], 'c');
-%     end
-%     if (meas == 3) 
-%         plot([x(1,t) x(1,t)+y(1,t)*cos(y(2,t)+x(3,t))], [x(2,t) x(2,t)+y(1,t)*sin(y(2,t)+x(3,t))], 'c');
-%     end
-%     for dd = 1:D
-%      plot(X(1, dd), X(2, dd), 'b.')
-%     end
-%     axis equal
-%     axis([-5 7 -2 8]);
-%     title('Particle Filter Localization')
-%     
-%     % record plot frame
-%     if (makemovie) 
-%         writeVideo(vidObj, getframe(gca)); 
-%     end
-% end
-% 
-% % close video object
-% if (makemovie) 
-%     close(vidObj); 
-% end
+% Main loop
+for t = 2:length(T)    
+    % update state
+    e = RE * sqrt(Re) * randn(n, 1);
+    x(:, t) = [x(1, t - 1) + u(1, t) * cos(x(3, t - 1)) * dt;
+              x(2, t - 1) + u(1, t) * sin(x(3, t - 1)) * dt;
+              x(3, t - 1) + u(2, t) * dt] + e;
+    
+    % take measurement
+    % pick feature
+    mf(:,t) = closestfeature(map,x(:,t));
+    % Select a motion disturbance
+    d = QE * sqrt(Qe) * randn(m, 1);
+    % determine measurement
+    switch(meas) 
+        case 1  % 1 - range
+            r = range(mf(1, t), x(1, t), mf(2, t), x(2, t));
+            y(:, t) = r + d;
+        case 2  % 2 - bearing
+            b = bearing(mf(1, t), mf(2, t), x(1, t), x(2, t), x(3, t));
+            y(:, t) = b + d;
+        case 3  % 3 - both
+            r = range(mf(1, t), x(1, t), mf(2, t), x(2, t));
+            b = bearing(mf(1, t), mf(2, t), x(1, t), x(2, t), x(3, t));
+            y(:, t) = [r; b] + d;
+    end
+
+    % particle filter estimation
+    for dd = 1:D
+        e = RE * sqrt(Re) * randn(n, 1);
+        Xp(:, dd) = [X(1, dd) + u(1, t) * cos(X(3, dd)) * dt;
+                    X(2, dd) + u(1, t) * sin(X(3, dd)) * dt;
+                    X(3, dd) + u(2, t) * dt] + e;
+        switch(meas)
+            case 1  % 1 - range
+                r = range(mf(1, t), Xp(1, dd), mf(2, t), Xp(2, dd));
+                hXp = r + d;
+            case 2  % 2 - bearing
+                b = bearing(mf(1, t), mf(2, t), Xp(1, t), Xp(2, t), Xp(3, t));
+                hXp = b + d;
+            case 3  % 3 - both
+                r = range(mf(1, t), Xp(1, dd), mf(2, t), Xp(2, dd));
+                b = bearing(mf(1, t), mf(2, t), Xp(1, dd), Xp(2, dd), Xp(3, dd));
+                hXp = [r; b] + d;
+        end
+        w(dd) = max(1e-8, mvnpdf(y(:, t), hXp, Q));
+    end
+    W = cumsum(w);
+
+    for dd = 1:D
+        seed = max(W) * rand(1);
+        X(:, dd) = Xp(:, find(W > seed, 1));
+    end
+
+    muParticle = mean(X);
+    SParticle = var(X);    muParticle_S(1, :, t) = muParticle;
+    
+    % record
+    if (makemovie) 
+        % plot results
+        plot_results(1, meas, map, t, x, X, y, mf, D);
+        
+        % record plot frame
+        writeVideo(vidObj, getframe(gca)); 
+    end
+end
+
+% close video object
+if (makemovie) 
+    close(vidObj); 
+end
