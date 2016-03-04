bd81ecda53a0750070ced71be3ff6261d9f86a42
Brent Komer <brent.komer@gmail.com>
2016-03-04 17:50:57 -0500

    Adding optional u parameter

--- matlab_simulation/05-estimation/models/airplane_radar/airplane_radar_linearized_motion_model.m
+++ matlab_simulation/05-estimation/models/airplane_radar/airplane_radar_linearized_motion_model.m
@@ -1,5 +1,5 @@
-function M = airplane_radar_linearized_motion_model(~, dt)
-    if nargin == 1
+function M = airplane_radar_linearized_motion_model(~, ~, dt)
+    if ~exist('dt', 'var')
         dt = 0.1;
     end
     Ad = [ 1 dt 0 ; 0 1 0; 0 0 1];
--- matlab_simulation/05-estimation/models/airplane_radar/airplane_radar_measurement_model.m
+++ matlab_simulation/05-estimation/models/airplane_radar/airplane_radar_measurement_model.m
@@ -1,3 +1,3 @@
-function M = airplane_radar_measurement_model(mup)
+function M = airplane_radar_measurement_model(mup, ~)
     M = sqrt(mup(1)^2 + mup(3)^2);
 end
