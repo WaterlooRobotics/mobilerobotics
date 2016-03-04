84af46a6a39a732bc01d6f2ffb12a725421b6831
Brent Komer <brent.komer@gmail.com>
2016-03-03 19:42:48 -0500

    Defining motion and measurement models for example
    Matlab doesn't allow more than one function in a file for some reason,
    so these need to be separated

---
+++ matlab_simulation/05-estimation/models/airplane_radar/airplane_radar_linearized_measurement_model.m
@@ -0,0 +1,3 @@
+function M = airplane_radar_linearized_measurement_model(mup)
+    M = [(mup(1))/(sqrt(mup(1)^2 + mup(3)^2)) 0 (mup(3))/(sqrt(mup(1)^2 + mup(3)^2))];
+end
---
+++ matlab_simulation/05-estimation/models/airplane_radar/airplane_radar_motion_model.m
@@ -0,0 +1,7 @@
+function M = airplane_radar_motion_model(mu, dt)
+    if nargin == 1
+        dt = 0.1;
+    end
+    Ad = [ 1 dt 0 ; 0 1 0; 0 0 1];
+    M = Ad*mu;
+end
