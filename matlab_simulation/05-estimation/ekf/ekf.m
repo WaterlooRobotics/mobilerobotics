8470de4a68d9466a3cf39f64265a054858542acc
Brent Komer <brent.komer@gmail.com>
2016-03-04 17:50:57 -0500

    Adding optional u parameter

--- matlab_simulation/05-estimation/ekf/ekf.m
+++ matlab_simulation/05-estimation/ekf/ekf.m
@@ -1,4 +1,4 @@
-function [mu, S, mup, K] = ekf(mu, S, y, motion_model, measurement_model, linearized_motion_model, linearized_measurement_model, Q, R)
+function [mu, S, mup, K] = ekf(mu, S, y, motion_model, measurement_model, linearized_motion_model, linearized_measurement_model, Q, R, u)
 % EFK - Run an iteration of an Extended Kalman Filter
 %
 % Inputs:
@@ -12,6 +12,7 @@ function [mu, S, mup, K] = ekf(mu, S, y, motion_model, measurement_model, linear
 % linearized_measurement_model - reference to a function for the linearization of the measurement model
 % Q - measurement disturbance model
 % R - motion disturbance model
+% u - system input at the current timestep
 %
 % Outputs:
 %
@@ -20,13 +21,17 @@ function [mu, S, mup, K] = ekf(mu, S, y, motion_model, measurement_model, linear
 % mup - predicted mean
 % K - Kalman gain
 
+    if ~exist('u', 'var')
+        u = 0;
+    end
+
     %%% Prediction update
     
     % Propagate mu through the nonlinear motion model
-    mup = motion_model(mu);
+    mup = motion_model(mu, u);
     
     % Linearize motion model at the predicted mean
-    G = linearized_motion_model(mup);
+    G = linearized_motion_model(mup, u);
     
     % Compute predicted covariance
     Sp = G*S*G' + R;
@@ -35,13 +40,13 @@ function [mu, S, mup, K] = ekf(mu, S, y, motion_model, measurement_model, linear
     %%% Measurement update
     
     % Linearize measurement model at the predicted mean
-    H = linearized_measurement_model(mup);
+    H = linearized_measurement_model(mup, u);
 
     % Compute Kalman gain
     K = Sp*H'*inv(H*Sp*H'+Q);
     
     % Update mean using the nonlinear measurement model
-    mu = mup + K*(y-measurement_model(mup));
+    mu = mup + K*(y-measurement_model(mup, u));
     
     % Update the covariance based on the measurement model
     S = (eye(length(mu))-K*H)*Sp;
--- matlab_simulation/05-estimation/models/airplane_radar/airplane_radar_motion_model.m
+++ matlab_simulation/05-estimation/models/airplane_radar/airplane_radar_motion_model.m
@@ -1,5 +1,5 @@
-function M = airplane_radar_motion_model(mu, dt)
-    if nargin == 1
+function M = airplane_radar_motion_model(mu, ~, dt)
+    if ~exist('dt', 'var')
         dt = 0.1;
     end
     Ad = [ 1 dt 0 ; 0 1 0; 0 0 1];
