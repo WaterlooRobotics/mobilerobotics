e7deca9719215c56ad5ce4a8b7d2b1a64115d737
Brent Komer <brent.komer@gmail.com>
2016-03-03 19:43:20 -0500

    Moving EFK into a subfolder within estimation

---
+++ matlab_simulation/05-estimation/ekf/ekf.m
@@ -0,0 +1,18 @@
+% Inputs: prior state prediction, prior covariance matrix, measurement,
+% motion model, measurement model, Q, and R
+
+function [mu, S, mup, K] = ekf(mu, S, y, motion_model, measurement_model, linearized_motion_model, linearized_measurement_model, Q, R)
+
+    % Prediction update
+    mup = motion_model(mu);
+    G = linearized_motion_model(mup);
+    Sp = G*S*G' + R;
+
+    % Linearize measurement model at the predicted mean
+    H = linearized_measurement_model(mup);
+
+    % Measurement update
+    K = Sp*H'*inv(H*Sp*H'+Q);
+    mu = mup + K*(y-measurement_model(mup));
+    S = (eye(length(mu))-K*H)*Sp;
+end
