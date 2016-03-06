0e20e6136781581ee437f3dfd358f3c33d5daeda
Baruch Chau <baruchchau@gmail.com>
2016-03-05 23:46:08 -0500

    Create CompareEKF_UKF.m function
    Moved the EKF verses UKF error plotting code to a new function

---
+++ matlab_simulation/code v1.0/ME597-5-Unscented Kalman Filter/CompareEKF_UKF.m
@@ -0,0 +1,13 @@
+function CompareEKF_UKF(T,x,mu_S,mu_Su )
+%UNTITLED5 Summary of this function goes here
+%   Detailed explanation goes here
+    figure(2);clf; hold on;
+    e = sqrt((x(1,2:end)-mu_S(1,2:end)).^2+(x(2,2:end)-mu_S(2,2:end)).^2);
+    plot(T(2:end),e,'b', 'LineWidth', 1.5);
+    eu = sqrt((x(1,2:end)-mu_Su(1,2:end)).^2+(x(2,2:end)-mu_Su(2,2:end)).^2);
+    plot(T(2:end),eu,'g', 'LineWidth', 1.5);
+    title('Position Estimation Errors for EKF and UKF')
+    xlabel('Time (s)');
+    ylabel('X-Z Position Error (m)');
+    legend('EKF','UKF');
+end
