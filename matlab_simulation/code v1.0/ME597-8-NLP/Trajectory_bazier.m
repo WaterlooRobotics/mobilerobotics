890d650099b272c8b685d675627e455705e6e70b
neginlashkari <nlashkar@uwaterloo.ca>
2016-03-07 11:32:35 -0500

    Trajectory generator
    This code generates trajectories for robots based on bezier curves

--- matlab_simulation/code v1.0/ME597-8-NLP/Trajectory_bazier.m
+++ matlab_simulation/code v1.0/ME597-8-NLP/Trajectory_bazier.m
@@ -1,5 +1,20 @@
+%% This file develops a desired trajectory using Bazier Curves.
+%% P is a set of points including start point, bazier control point and target.
+
 function xdT=Trajectory_bazier(xd_start,xd_end,dt,TTot)
 xd_midpoint=(xd_end(1)-xd_start(1)).*(rand(1,2));
 P=[xd_start;xd_start+xd_midpoint;xd_end];
-BezierCurve(P); %generates Bz_fun
+
+syms g
+Np = size(P, 1); 
+
+for i = 1:Np
+   Bp(:,i) = nchoosek(Np,i-1).*(g.^(i-1)).*((1-g).^(Np-i+1)); %Bp is the Bernstein polynomial value
+end
+
+Bp1= (nchoosek(Np,Np).*(g.^Np)).';
+
+Sp= simplify(Bp*P + Bp1*P(Np,:));
+Bz_fun=matlabFunction(Sp);
+
 xdT=[Bz_fun(([0:dt:(TTot)*dt]./(TTot*dt)).'),zeros(length([0:dt:(TTot*dt)]),1)];
