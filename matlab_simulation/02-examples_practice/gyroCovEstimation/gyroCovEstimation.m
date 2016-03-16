905f3ece2cd4969fe52eb4ee51be22e082d7d280
LordBismaya <bismaya.iitkgp@gmail.com>
2016-03-15 21:21:02 -0400

    Commit after review. Following Changes 1. Updated the MobileRoboticsSetup.m to add the 11-datasets folder 2. Removed a duplicate copy of this same file 3. Added the Info about this file in the top comments 4. Changed the titles of the error plots which were earlier the same. 5. Improved formatting 6. Separate Algo and Plot sections. 7. Changed the Error plot to match to that of the slides.

--- matlab_simulation/02-examples_practice/gyroCovEstimation/gyroCovEstimation.m
+++ matlab_simulation/02-examples_practice/gyroCovEstimation/gyroCovEstimation.m
@@ -1,29 +1,27 @@
 %% A Matlab program to calculate the convariances from a Gyroscope Data.
 % It demonstrates the following aspects:
-% -1. Load gyroDataset from the 11-datasets directory
-% -2. Adds Gaussian Noise to the dataset
-% -3. Filters the Dataset using a moving average filter
-% -4. Displays both the noisy and the filtered datasets for comparision
-% -5. Approximates the noise distribution that was added to the dataset
+% -0. Load gyroDataset from the 11-datasets directory
+% -1. Simulate a Gyro Data corrupted due to noise due to vehicle motion 
+% -2. Filter the Simulated gyro data
+% -3. Estimate the noise in the filtered data by comparing with original.
+% -4. Add this noise to filtered data to verify it matches the original
+% -5. Approximates the noise distribution 
 % ---------------------------------------------------------------------
-% ----Contributions in Ver.2.0
+% ----Contributions in Ver.3.0
 % -1. Vectorized the Gyro Data.
 % -2. More Compact Code.
 % -3. Removed repeatations
 % -4. Completed the Error Distribution Comparison at the bottom.
-% -5. Commented wherever possible
-% ----Contributions in Ver.3.0 after review
-% -1. Updated the MobileRoboticsSetup.m to add the 11-datasets folder
-% -2. Removed a duplicate copy of this same file
-% -3. Added the Info about this file in the top comments
-% -4. Changed the titles of the error plots which were earlier the same.
-% -5.  
-% Author: Bismaya Sahoo
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% -5. Comments/Description wherever possible
+% -6. All Plots in the same subplot for ease of viewing
+% -7. Seprate ALGO and PLOT section for neatness
+% -8. Clear explanation of all the steps
+% Author: Bismaya Sahoo EMAIL:bsahoo@uwaterloo.ca
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 %% Initializations
 clear all; close all; clc;
 RAD2DEG = 180/pi;
-
 tmin = 10; % Put Start_time here in seconds
 tmax = 20; % Put Stop_time here in seconds
 
@@ -31,50 +29,42 @@ tmax = 20; % Put Stop_time here in seconds
 load('gyroData.mat'); % Load the Gyro Dataset.
 G_data = [gx gy gz]; % Vectorize the Dataset. G(:,1)=gx,G(:,2)=gy;G(:,3)=gz;
 G_data = G_data*RAD2DEG; % Convert the Dataset to degrees.
-dataSet_length = length(G_data(:,1));
-time_Stamp = tg;
+% Assume underlying motion is negligible, and measurements are independent
+% This is our ground truth.Assume the original GyroData is accurate.
 
+S= cov(G_data);%Calculate the Covariance for reproduction of simulated data
+fprintf('The Covariance of the Original Dataset is:\n');disp(S);
+[S_vect,S_val] = eig(S); % S_vect: eigVectors, S_val: eigValues
+
+dataSet_length = length(G_data(:,1));
+time_Stamp = tg;%tg is the variable name for timeStamp in Data File.
 % Calculate Plotting interval
 t_start = find(time_Stamp>tmin,1);% Find the start location in the dataset
 t_stop = find(time_Stamp>tmax,1);% Find the stop location in the dataset
 
-%% Calculate Covariance and A noisy simulation
-% Assume underlying motion is negligible, and measurements are independent
-S= cov(G_data);
-[S_vect,S_val] = eig(S); % S_vect: eigVectors, S_val: eigValues
-
-% Create a reproduction of gyro data
+%% Run the process/simulation
+% STEP1: Simulate Gyro Data that includes vehicle motions
+% The reproduction would have the same cov but corrupted by noise.
 G_sim = S_vect*sqrt(S_val)*randn(3,dataSet_length);
 
-% Plot the data
-figure(1);clf;
-subplot(2,1,1); hold on;grid on;
-plot(time_Stamp(t_start:t_stop), G_data(t_start:t_stop,:));
-axis([tmin tmax -60 60]);title('Original Data');xlabel('Time(s)');ylabel('Gyro Rate (deg/s)');legend('Gx','Gy','Gz')
-subplot(2,1,2); hold on;grid on;
-plot(tg(t_start:t_stop), G_sim(:,(t_start:t_stop)));
-axis([tmin tmax -60 60]);xlabel('Time(s)');ylabel('Rate (deg/s)');title('Unfiltered simulation');legend('Gx_{Sim}','Gy_{Sim}','Gz_{Sim}');
-
-%% Filter the Dataset 
+% STEP2: Filter the Gyro Sensor on our robot with a moving avg filter.
 filter_length = 3; % size of the moving avg filter.
 G_filtered=filter(ones(1,filter_length)/filter_length,1,G_data); %Filter the Data.
+
+% STEP3: Calculate the error of our filtered estimate from the original data.
+% This error when added to our estimate should give estimated gyro data.
 G_err = G_data-G_filtered; %Calculate the error
-
-Sf_err = cov(G_err);
+Sf_err = cov(G_err); % Calculate the error covariance
+fprintf('The Covariance of the Noise added to the filtered dataset is:\n');disp(Sf_err);
 [SErr_Vec, SErr_val]=eig(Sf_err);
-% Create a reproduction of gyro data
-G_simf = SErr_Vec*sqrt(SErr_val)*randn(3,dataSet_length);
-% Plot the Filtered Data
-figure(2);clf;
-subplot(2,1,1); hold on;
-plot(time_Stamp(t_start:t_stop), G_data(t_start:t_stop,:));
-axis([tmin tmax -60 60]);title('Original Data');xlabel('Time(s)');ylabel('Gyro Rate (deg/s)');legend('Gx','Gy','Gz')
-subplot(2,1,2); hold on;
-plot(time_Stamp(t_start:t_stop), G_filtered(t_start:t_stop,:)'+G_simf(:,t_start:t_stop));
-axis([tmin tmax -60 60]);xlabel('Time(s)');ylabel('Rate (deg/s)');title('Filtered simulation');legend('Gx_{Sim}','Gy_{Sim}','Gz_{Sim}');
 
-%% Error Distribution Comparison
-figure(3); clf; hold on
+% STEP 4: Create a reproduction of the error
+G_sim_err = SErr_Vec*sqrt(SErr_val)*randn(3,dataSet_length);
+
+% STEP 5: Calculate our final Gyro Estimate.
+G_out=G_filtered'+G_sim_err;
+
+% STEP 6: Estimate the Error Distribution
 bins = 100;
 xvec = -50:.1:50;
 [Nex, Xex] = hist(G_err(:,1),bins);
@@ -83,15 +73,34 @@ xvec = -50:.1:50;
 binsizeX = Xex(2)-Xex(1);
 binsizeY = Xey(2)-Xey(1);
 binsizeZ = Xez(2)-Xez(1);
-
 % Compare to pdf, by multiplying by number of samples and by bin size
 % Num samples to convert to frequency distribution and bin size as smaller
 % bins will have fewer samples.
-
 Gex = normpdf(xvec,mean(G_err(:,1)),sqrt(Sf_err(1,1)))*length(G_err(:,1))*binsizeX;
 Gey = normpdf(xvec,mean(G_err(:,2)),sqrt(Sf_err(2,2)))*length(G_err(:,2))*binsizeY;
 Gez = normpdf(xvec,mean(G_err(:,3)),sqrt(Sf_err(3,3)))*length(G_err(:,3))*binsizeZ;
 
+
+%% Plot Zone
+% Plot the Original data
+figure(1);clf;
+subplot(2,2,1); hold on;grid on;
+plot(time_Stamp(t_start:t_stop), G_data(t_start:t_stop,:));
+axis([tmin tmax -60 60]);title('1.Original Data');xlabel('Time(s)');ylabel('Gyro Rate (deg/s)');legend('Gx','Gy','Gz')
+%Plot the Simulated Data with vehicle motions
+subplot(2,2,2); hold on;grid on;
+plot(tg(t_start:t_stop), G_sim(:,(t_start:t_stop)));
+axis([tmin tmax -60 60]);title('2.Unfiltered Data');xlabel('Time(s)');ylabel('Gyro Rate (deg/s)');legend('Gx_{Sim}','Gy_{Sim}','Gz_{Sim}');
+% Plot the Filtered Data
+subplot(2,2,3); hold on;grid on;
+plot(time_Stamp(t_start:t_stop), G_filtered(t_start:t_stop,:));
+axis([tmin tmax -60 60]);title('3.Filtered Data');xlabel('Time(s)');ylabel('Gyro Rate (deg/s)');legend('Gx_f','Gy_f','Gz_f')
+% Plot the Predicted Gyro Data
+subplot(2,2,4); hold on;grid on;
+plot(time_Stamp(t_start:t_stop), G_out(:,t_start:t_stop));
+axis([tmin tmax -60 60]);title('4.Estimated Data');xlabel('Time(s)');ylabel('Gyro Rate (deg/s)');legend('Gx_{pred}','Gy_{pred}','Gz_{pred}');
+% Plot the Error Distribution
+figure(2); clf; hold on
 subplot(3,1,1);bar(Xex,Nex);hold on;plot(xvec,Gex,'r','LineWidth',2);
 title('Error Distribution in X');xlabel('Error Value');ylabel('Histogram Count');
 subplot(3,1,2);bar(Xey,Ney);hold on;plot(xvec,Gey,'r', 'LineWidth',2);
