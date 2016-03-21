56bad8be183908f992ae76f507978d01f5f86faa
Brent Komer <brent.komer@gmail.com>
2016-03-20 21:28:20 -0400

    Updating based on suggestions

--- matlab_simulation/05-estimation/models/airplane_radar/airplane_radar_linearized_motion_model.m
+++ matlab_simulation/05-estimation/models/airplane_radar/airplane_radar_linearized_motion_model.m
@@ -1,7 +1,12 @@
-function M = airplane_radar_linearized_motion_model(~, ~, dt)
+function A = airplane_radar_linearized_motion_model(~, ~, dt)
+% Implements the linearized motion model on slide 112 of EstimationI
+% Airplane flies horizontally with a constant velocity
+% The model is already linear, so no inputs are needed
+% dt - optional timestep
+% A - the A matrix of the linearized model
     if ~exist('dt', 'var')
         dt = 0.1;
     end
     Ad = [ 1 dt 0 ; 0 1 0; 0 0 1];
-    M = Ad;
+    A = Ad;
 end
--- matlab_simulation/05-estimation/models/airplane_radar/airplane_radar_measurement_model.m
+++ matlab_simulation/05-estimation/models/airplane_radar/airplane_radar_measurement_model.m
@@ -1,3 +1,8 @@
-function M = airplane_radar_measurement_model(mup, ~)
-    M = sqrt(mup(1)^2 + mup(3)^2);
+function yp = airplane_radar_measurement_model(mup, ~)
+% Implements the measurement model on slide 113 of EstimationI
+% The radar returns the straight-line distance from a point on the ground
+% to the airplane position
+% mup - predicted state
+% yp - predicted measurement
+    yp = sqrt(mup(1)^2 + mup(3)^2);
 end
