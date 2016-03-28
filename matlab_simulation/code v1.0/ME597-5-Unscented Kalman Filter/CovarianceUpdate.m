0e643a67815d751d31f36b17407b290a2e07fa50
Baruch Chau <baruchchau@gmail.com>
2016-03-28 17:15:51 -0400

    Relocating to proper location

--- matlab_simulation/code v1.0/ME597-5-Unscented Kalman Filter/CovarianceUpdate.m
+++
@@ -1,6 +0,0 @@
-function [K_u,mu_u,S_u] = CovarianceUpdate(t,Sxy_u,Sy_u,Sp_u,mup_u,y,y_u)
-%Updates the mean and covaraince with the cross covariance values
-    K_u = Sxy_u*inv(Sy_u);
-    mu_u = mup_u + K_u*(y(:,t)-y_u);
-    S_u = Sp_u - K_u*Sy_u*K_u';
-end
