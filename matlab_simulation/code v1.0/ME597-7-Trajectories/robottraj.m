1341727df520470ab67e8a57f584be96f7c0ad87
yshiyi <s268yang@uwaterloo.ca>
2016-03-31 17:00:15 -0400

    Add two new motions and plot multiple nudges and swerves

--- matlab_simulation/code v1.0/ME597-7-Trajectories/robottraj.m
+++ matlab_simulation/code v1.0/ME597-7-Trajectories/robottraj.m
@@ -1,4 +1,11 @@
 % Robot trajectories
+% This code only focused on generating different trajectories. 
+% Path planning is not considered.
+% This code can plot six different trajectories:
+% spiral, nudges, swerves, corner, lane change, obstacle avoid.
+% In addition, it can plot the trajectory which is defined by users.
+% Users are also allowed to give any initial condition except number 6.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear;
 close;
 clc;
@@ -11,7 +18,9 @@ T = 0:dt:Tmax;
 % Let user choose the input trajectory
 disp('What is the input trajectory?')
 disp('1: Spiral; 2: Nudges; 3: Swerves; 4: Corner;')
-disp('5: User defined motion model')
+disp('5: A single lane change')
+disp('6: Get around an obstacle')
+disp('7: User defined motion model')
 prompt = '';
 index = input(prompt);
 
@@ -19,11 +28,17 @@ index = input(prompt);
 % Input index should be between 1-5
 index = input_check(index);
 
-% Let user define an initial condition
-disp('Please give an initial condition, i.e. [1 1 1].')
-disp('x1: x-position; x2: y-position; x3: heading angle.')
-prompt = '';
-x0 = input(prompt)';
+if index == 6
+    % In this program we don't focus on the path planning.
+    % The initial condition for 6 is fixed.
+    x0 = [1; 1; 1];
+else
+    % Let user define an initial condition
+    disp('Please give an initial condition, i.e. [1 1 1].')
+    disp('x1: x-position; x2: y-position; x3: heading angle.')
+    prompt = '';
+    x0 = input(prompt)';
+end
 
 % Define the states
 % xd1: x-position; xd2: y-position; xd3: heading angle
@@ -39,20 +54,31 @@ switch index
         % Spiral
         [xddot, xd]=spiral(T, dt, xd, xddot);
         plot_figure(xd, T, index);
-        
     case 2
         % Nudges
-        [xddot, xd]=nudges(T, dt, xd, xddot);
-        plot_figure(xd, T, index);
+        for i = 1: 5
+            [xddot, xd]=nudges(T, dt, xd, xddot, i);
+            plot_figure(xd, T, index);
+        end
     case 3
         % Swerves
-        [xddot, xd]=swerves(T, dt, xd, xddot);
-        plot_figure(xd, T, index);
+        for i = 1: 5
+            [xddot, xd]=swerves(T, dt, xd, xddot, i);
+            plot_figure(xd, T, index);
+        end
     case 4
         % Corner
         [xddot, xd]=corner(T, dt, xd, xddot);
         plot_figure(xd, T, index);
     case 5
+        % A single lane change
+        [xddot, xd]=lane_change(T, dt, xd, xddot);
+        plot_figure(xd, T, index);
+    case 6
+        % Get around an obstacle
+        [xddot, xd]=obstacle(T, dt, xd, xddot);
+        plot_figure(xd, T, index);
+    case 7
         % User defined motion model
         [xddot, xd] = user_define_motion_model(T, dt, xd, xddot);
         plot_figure(xd, T, index);
