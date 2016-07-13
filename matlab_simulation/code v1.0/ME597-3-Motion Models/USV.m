d1bd839ef59b9467dff390fb262525c65b9ef90a
unknown <mcote@CPR00572.clearpath.ai>
2016-07-12 22:05:22 -0400

    -Added motion_model_example, - Added a rough calculation to take into account the effect of drag  for the AUV and SUV model - The AUV and USV model's now pass previous velocity information

--- matlab_simulation/code v1.0/ME597-3-Motion Models/USV.m
+++ matlab_simulation/code v1.0/ME597-3-Motion Models/USV.m
@@ -1,16 +1,32 @@
 function xcur = USV(xprev,Tl, Tr, m, ll, lr, dt)
 %% Motion model for a 2 thruster USV, Such as Clearpath Robotics Heron Platform 
-%  xprev = [x,y,theta]'right thrust and left thrust are denoted as Tl and Tr, mass of m,
+%  xprev = [x,y,theta,vx,vy,vtheta]'right thrust and left thrust are denoted as Tl and Tr, mass of m,
 %  perpendicular distance from thruster to cog is ll and lr for right thruster and left thruster respectivly
 %  as well as the timestep dt. It returns the new robot pose. 
 
+%Very rough drag calculated for Clearpath's Heron, comment this out and remove from
+%velocity equations if using a diffrent USV, or sub in your own vehicles
+%properties.Note drag for theta was not modeled, so rotational speed can
+%get very large
+%A~0.31364m^2, assuming drag coeffecient of ~0.7
+
+F_drag=-0.5*1000*0.7*0.31364*xprev(4)^2;
+
+if xprev(4)<0
+    F_drag=-F_drag;
+end
+
+%update velocities
+v_x=xprev(4) + ((Tl/m+Tr/m+F_drag/m)*dt);
+v_theta=xprev(6) + ((Tl*ll-Tr*lr)/m)*dt;
 
 % Motion increment in the body frame
-dx_b = dt*[Tl/m+Tr/m 0 (Tl*ll-Tr*lr)/m]';
+dx_b = [v_x*dt 0 v_theta*dt]';
 
 % Rotation matrix for conversion from body to inertial frame
 R = rot(xprev(3),3); 
 
 % Robot state update in inertial frame
-xcur = xprev + R*dx_b;
+xcur = [xprev(1:3) + R*dx_b; v_x;0;v_theta];
+
 
