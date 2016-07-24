adc98656f891a8046ad335e5e1bc69daf4a09d10
Ajay Singh Chhonker <ajaysingh.chhonker@gmail.com>
2016-07-25 00:02:20 +0530

    Optimized EKF
    No. of argument to EKF function reduced.
    EKF function made more generic.
    Axis and legend added in video.

--- matlab_simulation/code v1.0/ME597-5-ExtendedKalmanFilter/EKF.m
+++ matlab_simulation/code v1.0/ME597-5-ExtendedKalmanFilter/EKF.m
@@ -1,24 +1,19 @@
-function [mu,S,K,mup] = EKF(mu,S,w,v,dt,y,motion_model,sensor_model,linearize_motion_model,linearize_sensor_model,R,Q)
+function [mu,S,K,mup] = EKF(g,Gt,Ht,S,Y,sensor_model,R,Q)
 %% Extended Kalman Filter.
-%This function takes mu, Covariance, motion and sensor model and their derivaties as input
+%This function takes Covariance, motion and sensor model and their derivaties and noise as input
 %and returns Kalman Gain, Updated mu & Covariance and predicted mu.
 
-
 %% Extended Kalman Filter Estimation
-    n=length(mu);
-    %y=sensor_model(mu);
-
     % Linearization
-    Gt = linearize_motion_model(mu,v,dt);
-    Ht = linearize_sensor_model(mu);
+    % taking linearized models as input so as to make this function more generic
     
     % Prediction
-    mup = motion_model(mu,w,v,dt);
+    mup = g;
+    n=length(mup);
     Sp = Gt*S*Gt' + R;
     
     % update
     K = Sp*Ht'*inv(Ht*Sp*Ht'+Q);
-    mu = mup + K*(y-sensor_model(mup));
+    mu = mup + K*(Y-sensor_model(mup));
     S = (eye(n)-K*Ht)*Sp;
-
 end
--- matlab_simulation/code v1.0/ME597-5-ExtendedKalmanFilter/EKF_omnibot_example.m
+++ matlab_simulation/code v1.0/ME597-5-ExtendedKalmanFilter/EKF_omnibot_example.m
@@ -1,5 +1,7 @@
 % Extended Kalman filter example
-clear;clc;
+clc;
+clear all;
+close all;
 
 %% Create AVI object
 makemovie = 1;
@@ -49,6 +51,9 @@ for t=2:length(T)
     % Update state
     x_ideal(:,t) = omnibot_motion_model(x_ideal(:,t-1),w,v,dt);
     x(:,t) = omnibot_motion_model(x(:,t-1),w,v,dt) + e;
+    g = x_ideal(:,t);
+    Gt = omnibot_linearize_motion_model(x_ideal(:,t-1),v,dt);
+    Ht = omnibot_linearize_sensor_model(x_ideal(:,t-1));
 
     % Take measurement
     % Select a motion disturbance
@@ -57,31 +62,38 @@ for t=2:length(T)
     y(:,t) = omnibot_sensor_model(x_ideal(:,t)) + d;
     Y=y(:,t);
 
-
     %% Extended Kalman Filter Estimation
-    [mu,S,K,mup] = EKF(mu,S,w,v,dt,Y,@omnibot_motion_model,@omnibot_sensor_model,...
-        @omnibot_linearize_motion_model,@omnibot_linearize_sensor_model,R,Q);
+    [mu,S,K,mup] = EKF(g,Gt,Ht,S,Y,@omnibot_sensor_model,R,Q);
 
     % Store results
     mup_S(:,t) = mup;
     mu_S(:,t) = mu;
-    %K_S(:,t) = K;
-
-
+    
     %% Plot results
     figure(1);clf; hold on;
-    %plot(0,0,'bx', 'MarkerSize', 6, 'LineWidth', 2)
-    %plot([20 -1],[0 0],'b--')
     plot(x(1,2:t),x(2,2:t), 'ro--')
     plot(mu_S(1,2:t),mu_S(2,2:t), 'bx--')
     mu_pos = [mu(1) mu(2)];
     S_pos = [S(1,1) S(1,2); S(2,1) S(2,2)];
     error_ellipse(S_pos,mu_pos,0.75);
     error_ellipse(S_pos,mu_pos,0.95);
-    title('True state and belief')
+    
+    %for printing title and legend in video
+    text(-3,1.5,'True state and belief');
+    text(4.7,1,'True Path','Color','red');  %printing text is faster than printing legend
+    text(4.7,1.7,'EKF path','Color','blue');
+    
+    %for printing axis in video
+    for i=-8:2:8
+        text(i-0.2,-13.5,num2str(i));
+    end
+    for j=-14:2:2
+        text(-7.7,j,num2str(j));
+    end
     axis equal
     axis([-8 8 -14 2])
-    legend('True Path', 'EKF path');
+    pause(0.0001);
+
     if (makemovie) writeVideo(vidObj, getframe(gca)); end
 
 end
