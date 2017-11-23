004693196861f9a7a243065363abec0a3b7a96ca
Steven Waslander <stevenw@uwaterloo.ca>
2017-11-23 11:19:36 -0500

    Moved scanner sensor inverse model example.

--- matlab_simulation/01-examples_lecture/mr-4-scanner/example_scanner.m
+++ matlab_simulation/01-examples_lecture/mr-4-scanner/example_scanner.m
@@ -8,6 +8,9 @@ m = 0.5*ones(M,N); %map
 alpha = 1; % Distance about measurement to fill in
 beta = 0.01; % Angle beyond which to exclude 
 
+p_occ = 0.7; % Probability for occupied cells
+p_free = 0.3 % Probability for free cells
+
 % Robot location
 x = 25;
 y = 10;
@@ -17,8 +20,7 @@ rmax = 80;
 % Measurements
 meas_phi = [-.4:0.01:.4]; % heading
 meas_r = 40*ones(size(meas_phi)); % range
-
-m = inversescanner(M,N,x,y,theta,meas_phi,meas_r,rmax,alpha,beta);
+m = inversescanner(M,N,x,y,theta,meas_phi,meas_r,rmax,alpha,beta, p_occ, p_free);
 
 figure(2);clf;hold on;
 image(100*(1-m));
