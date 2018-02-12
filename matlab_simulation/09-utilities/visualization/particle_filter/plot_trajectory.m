f4371440e013e080cb386169090a44b8425c4035
Barry James Gilhuly <barry.gilhuly@uwaterloo.ca>
2018-02-12 15:11:36 -0500

    pf.m was not returning the updated sample set X.  particle_filter was using the original (t=0) X for all calculations.
    updated to use the revised call to kalman_filter() removing the extra
    parameters for frequency, example, and dt

--- matlab_simulation/09-utilities/visualization/particle_filter/plot_trajectory.m
+++ matlab_simulation/09-utilities/visualization/particle_filter/plot_trajectory.m
@@ -1,4 +1,4 @@
-function plot_trajectory(figure_index, T, x, y, mu_S, muP_S)
+function plot_trajectory(figure_index, T, x, y, mu_S, muP_S, M)
     figure(figure_index);
     clf;
     hold on;
@@ -11,6 +11,6 @@ function plot_trajectory(figure_index, T, x, y, mu_S, muP_S)
     plot(T, 2 * ones(size(T)), 'm--');
     plot(T, 10 * ones(size(T)), 'm--');
     
-    title('State and estimates, M=10');
+    title(sprintf('State and estimates, M=%d', M));
     legend('State', 'Kalman Estimate', 'Particle Estimate')
 end
